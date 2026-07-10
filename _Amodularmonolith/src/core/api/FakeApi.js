b22e9d797ab20d283d28c0ca7b91be185e7dfec7
Soc Basillote <dicecepe@gmail.com>
2026-07-10 16:43:30 +0800

    fakeApi setup

---
+++ _Amodularmonolith/src/core/api/FakeApi.js
@@ -0,0 +1,19 @@
+import { delay } from "./delay.js";
+
+export function FakeApi(storage) {
+  return {
+    async loadTodos() {
+      await delay(800);
+
+      return storage.load("todos") ?? [];
+    },
+
+    async saveTodos(todos) {
+      await delay(500);
+
+      storage.save("todos", todos);
+
+      return todos;
+    },
+  };
+}
